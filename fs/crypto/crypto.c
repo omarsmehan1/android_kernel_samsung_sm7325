8a7c0fa3978a8fd4b15d1b28d38832f643a8abe1
Atakan <ata.kaner03@gmail.com>
2025-02-15 19:23:13 +0300

    Treewide: Remove SDP and DualDAR
    For easier upstream merging + we can merge them later on.
    
    Change-Id: Ie06b592b1fa4e1c1ab81cac0c6561173eb43e4a5

--- fs/crypto/crypto.c
+++ fs/crypto/crypto.c
@@ -27,9 +27,6 @@
 #include <linux/ratelimit.h>
 #include <crypto/skcipher.h>
 #include "fscrypt_private.h"
-#ifdef CONFIG_FSCRYPT_SDP
-#include "sdp/sdp_crypto.h"
-#endif
 
 static unsigned int num_prealloc_crypto_pages = 32;
 
@@ -113,9 +110,6 @@ int fscrypt_crypt_block(const struct inode *inode, fscrypt_direction_t rw,
 	struct fscrypt_info *ci = inode->i_crypt_info;
 	struct crypto_skcipher *tfm = ci->ci_key.tfm;
 	int res = 0;
-#ifdef CONFIG_FSCRYPT_SDP
-	sdp_fs_command_t *cmd = NULL;
-#endif
 
 	if (WARN_ON_ONCE(len <= 0))
 		return -EINVAL;
@@ -145,26 +139,6 @@ int fscrypt_crypt_block(const struct inode *inode, fscrypt_direction_t rw,
 	if (res) {
 		fscrypt_err(inode, "%scryption failed for block %llu: %d",
 			    (rw == FS_DECRYPT ? "De" : "En"), lblk_num, res);
-#ifdef CONFIG_FSCRYPT_SDP
-		if (ci->ci_sdp_info) {
-			if (ci->ci_sdp_info->sdp_flags & SDP_DEK_IS_SENSITIVE) {
-				printk("Record audit log in case of a failure during en/decryption of sensitive file\n");
-				if (rw == FS_DECRYPT) {
-					cmd = sdp_fs_command_alloc(FSOP_AUDIT_FAIL_DECRYPT,
-					current->tgid, ci->ci_sdp_info->engine_id, -1, inode->i_ino, res,
-							GFP_KERNEL);
-				} else {
-					cmd = sdp_fs_command_alloc(FSOP_AUDIT_FAIL_ENCRYPT,
-					current->tgid, ci->ci_sdp_info->engine_id, -1, inode->i_ino, res,
-							GFP_KERNEL);
-				}
-				if (cmd) {
-					sdp_fs_request(cmd, NULL);
-					sdp_fs_command_free(cmd);
-				}
-			}
-		}
-#endif
 		return res;
 	}
 	return 0;
@@ -209,13 +183,6 @@ struct page *fscrypt_encrypt_pagecache_blocks(struct page *page,
 		       (offs >> blockbits);
 	unsigned int i;
 	int err;
-
-#ifdef CONFIG_DDAR
-	if (fscrypt_dd_encrypted_inode(inode)) {
-		// Invert crypto order. OEM crypto must perform after 3rd party crypto
-		return NULL;
-	}
-#endif
 
 	if (WARN_ON_ONCE(!PageLocked(page)))
 		return ERR_PTR(-EINVAL);
@@ -263,12 +230,6 @@ int fscrypt_encrypt_block_inplace(const struct inode *inode, struct page *page,
 				  unsigned int len, unsigned int offs,
 				  u64 lblk_num, gfp_t gfp_flags)
 {
-#ifdef CONFIG_DDAR
-	if (fscrypt_dd_encrypted_inode(inode)) {
-		// Invert crypto order. OEM crypto must perform after 3rd party crypto
-		return 0;
-	}
-#endif
 	return fscrypt_crypt_block(inode, FS_ENCRYPT, lblk_num, page, page,
 				   len, offs, gfp_flags);
 }
@@ -301,13 +262,6 @@ int fscrypt_decrypt_pagecache_blocks(struct page *page, unsigned int len,
 		       (offs >> blockbits);
 	unsigned int i;
 	int err;
-
-#ifdef CONFIG_DDAR
-	if (fscrypt_dd_encrypted_inode(inode)) {
-		// Invert crypto order. OEM crypto must perform after 3rd party crypto
-		return 0;
-	}
-#endif
 
 	if (WARN_ON_ONCE(!PageLocked(page)))
 		return -EINVAL;
@@ -345,12 +299,6 @@ int fscrypt_decrypt_block_inplace(const struct inode *inode, struct page *page,
 				  unsigned int len, unsigned int offs,
 				  u64 lblk_num)
 {
-#ifdef CONFIG_DDAR
-	if (fscrypt_dd_encrypted_inode(inode)) {
-		// Invert crypto order. OEM crypto must perform after 3rd party crypto
-		return 0;
-	}
-#endif
 	return fscrypt_crypt_block(inode, FS_DECRYPT, lblk_num, page, page,
 				   len, offs, GFP_NOFS);
 }
@@ -442,11 +390,6 @@ static int __init fscrypt_init(void)
 	if (err)
 		goto fail_free_info;
 
-#ifdef CONFIG_FSCRYPT_SDP
-	sdp_crypto_init();
-	if (!fscrypt_sdp_init_sdp_info_cachep())
-		goto fail_free_info;
-#endif
 	return 0;
 
 fail_free_info:
--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -32,10 +32,6 @@
 #include <trace/events/f2fs.h>
 #include <uapi/linux/f2fs.h>
 
-#ifdef CONFIG_FSCRYPT_SDP
-#include <linux/fscrypto_sdp_ioctl.h>
-#endif
-
 static vm_fault_t f2fs_filemap_fault(struct vm_fault *vmf)
 {
 	struct inode *inode = file_inode(vmf->vma->vm_file);
@@ -4433,26 +4429,6 @@ long f2fs_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		return f2fs_ioc_get_valid_node_count(filp, arg);
 	case F2FS_IOC_STAT_COMPRESS_FILE:
  		return f2fs_ioc_stat_compress_file(filp, arg);
-#ifdef CONFIG_FSCRYPT_SDP
-	case FS_IOC_GET_SDP_INFO:
-	case FS_IOC_SET_SDP_POLICY:
-	case FS_IOC_SET_SENSITIVE:
-	case FS_IOC_SET_PROTECTED:
-	case FS_IOC_ADD_CHAMBER:
-	case FS_IOC_REMOVE_CHAMBER:
-#ifdef CONFIG_SDP_KEY_DUMP
-	case FS_IOC_DUMP_FILE_KEY:
-	case FS_IOC_TRACE_FILE:
-#endif
-		return fscrypt_sdp_ioctl(filp, cmd, arg);
-#endif
-#ifdef CONFIG_DDAR
-	case F2FS_IOC_GET_DD_POLICY:
-	case F2FS_IOC_SET_DD_POLICY:
-	case FS_IOC_GET_DD_INODE_COUNT:
-	case FS_IOC_HAS_DD_POLICY: /* KNOX_SUPPORT_DAR_DUAL_DO */
-		return fscrypt_dd_ioctl(cmd, &arg, file_inode(filp));
-#endif
 	default:
 		return -ENOTTY;
 	}
@@ -4638,24 +4614,6 @@ long f2fs_compat_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	case F2FS_IOC_COMPRESS_FILE:
 	case F2FS_IOC_GET_VALID_NODE_COUNT:
 	case F2FS_IOC_STAT_COMPRESS_FILE:
-#ifdef CONFIG_FSCRYPT_SDP
-	case FS_IOC_GET_SDP_INFO:
-	case FS_IOC_SET_SDP_POLICY:
-	case FS_IOC_SET_SENSITIVE:
-	case FS_IOC_SET_PROTECTED:
-	case FS_IOC_ADD_CHAMBER:
-	case FS_IOC_REMOVE_CHAMBER:
-#ifdef CONFIG_SDP_KEY_DUMP
-	case FS_IOC_DUMP_FILE_KEY:
-	case FS_IOC_TRACE_FILE:
-#endif // End of CONFIG_SDP_KEY_DUMP
-#endif // End of CONFIG_FSCRYPT_SDP
-#ifdef CONFIG_DDAR
-	case F2FS_IOC_GET_DD_POLICY:
-	case F2FS_IOC_SET_DD_POLICY:
-	case FS_IOC_GET_DD_INODE_COUNT:
-	case FS_IOC_HAS_DD_POLICY: /* KNOX_SUPPORT_DAR_DUAL_DO */
-#endif
 		break;
 	default:
 		return -ENOIOCTLCMD;
