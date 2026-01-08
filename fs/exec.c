f16ca162d040dd33e2809406314e2b7d5708b40e
omarsmehan1 <omarsmehan890@gmail.com>
2026-01-08 19:01:44 +0300

    redoing susfsv2 again manually

--- fs/exec.c
+++ fs/exec.c
@@ -64,13 +64,12 @@
 #include <linux/compat.h>
 #include <linux/vmalloc.h>
 
-#include <linux/uaccess.h>
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
+#ifdef CONFIG_KSU_SUSFS
 #include <linux/susfs_def.h>
 #endif
 
+#include <linux/uaccess.h>
 #include <asm/mmu_context.h>
-#include <asm/tlb.h>
 
 #include <trace/events/task.h>
 #include "internal.h"
@@ -1746,9 +1745,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-extern bool susfs_is_sus_su_hooks_enabled __read_mostly;
-extern bool __ksu_is_allow_uid(uid_t uid);
+#ifdef CONFIG_KSU_SUSFS
+extern bool ksu_execveat_hook __read_mostly;
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
 				void *envp, int *flags);
 #endif
@@ -1769,15 +1771,18 @@ static int __do_execve_file(int fd, struct filename *filename,
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
+#ifdef CONFIG_KSU_SUSFS
 	if (likely(susfs_is_current_proc_umounted())) {
 		goto orig_flow;
 	}
-	if (likely(susfs_is_sus_su_hooks_enabled) &&
-		unlikely(__ksu_is_allow_uid(current_uid().val)))
-	{
+	+
+	if (unlikely(ksu_execveat_hook)) {
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
+
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 	}
+
 orig_flow:
 #endif
 
