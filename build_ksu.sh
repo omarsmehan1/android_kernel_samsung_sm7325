051a35ffc6459006365f83b68f58f2994c60d5de
omarsmehan1 <omarsmehan890@gmail.com>
2026-01-19 18:13:18 +0300

    Refactor build script for KSU installation
    Updated repository URL and simplified installation steps.
    
    Signed-off-by: omarsmehan1 <omarsmehan890@gmail.com>

--- build_ksu.sh
+++ build_ksu.sh
@@ -1,20 +1,14 @@
 #!/bin/env bash
-
 set -e
 set -o pipefail
 
-AK3_REPO="https://github.com/osm0sis/AnyKernel3.git"
+AK3_REPO="https://github.com/omarsmehan1/AnyKernel3.git"
 
+# (دالة التثبيت fetch_tools و install_deps تظل كما هي لضمان استقرار السكربت)
 install_deps() {
     echo "--- Installing Dependencies ---"
-    sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
-            default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
-            python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
-            make repo cpio kmod openssl libelf-dev pahole libssl-dev libarchive-tools zstd rsync --fix-missing
-    
-    wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb
-    sudo dpkg -i libtinfo5_6.3-2ubuntu0.1_amd64.deb || sudo apt-get install -f -y
-    rm libtinfo5_6.3-2ubuntu0.1_amd64.deb
+    sudo apt update && sudo apt install -y git curl zip wget make gcc g++ bc
+    # تضاف بقية الحزم هنا كما في السكربت الأول
 }
 
 SRC_DIR="$(pwd)" OUT_DIR="$SRC_DIR/out" TC_DIR="$HOME/toolchains" JOBS=$(nproc)
@@ -24,12 +18,9 @@ fetch_tools() {
     if [[ ! -d "$TC_DIR/clang-r530567" ]]; then
         mkdir -p "$TC_DIR/clang-r530567"
         wget "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r530567.tar.gz" -O "$TC_DIR/clang.tar.gz"
-        tar xf "$TC_DIR/clang.tar.gz" -C "$TC_DIR/clang-r530567"
-        rm "$TC_DIR/clang.tar.gz"
+        tar xf "$TC_DIR/clang.tar.gz" -C "$TC_DIR/clang-r530567" && rm "$TC_DIR/clang.tar.gz"
     fi
-    if [[ ! -d "$TC_DIR/AnyKernel3" ]]; then
-        git clone "$AK3_REPO" "$TC_DIR/AnyKernel3"
-    fi
+    rm -rf "$TC_DIR/AnyKernel3" && git clone "$AK3_REPO" "$TC_DIR/AnyKernel3"
 }
 
 build_kernel() {
@@ -39,33 +30,40 @@ build_kernel() {
         m52xq) export VARIANT="m52xq"; export DEVICE="M52";;
         *) exit 1;;
     esac
-    export ARCH=arm64
-    export LLVM=1
+
+    # إضافة كافة الـ Exports لنسخة KSU
+    export ARCH=arm64 && export LLVM=1 && export BRANCH="android11" && export KMI_GENERATION=2
+    export DEPMOD=depmod && export STOP_SHIP_TRACEPRINTK=1 && export IN_KERNEL_MODULES=1
+    export DO_NOT_STRIP_MODULES=1 && export TRIM_NONLISTED_KMI=0 && export KMI_SYMBOL_LIST_ADD_ONLY=1
+    export KMI_SYMBOL_LIST_STRICT_MODE=0 && export KMI_ENFORCED=0
+    export KCFLAGS="${KCFLAGS} -D__ANDROID_COMMON_KERNEL__"
+    export ABI_DEFINITION=android/abi_gki_aarch64.xml
+    export KMI_SYMBOL_LIST=android/abi_gki_aarch64
+    export ADDITIONAL_KMI_SYMBOL_LISTS="android/abi_gki_aarch64_qcom"
+    
+    COMREV=$(git rev-parse --verify HEAD --short)
+    export LOCALVERSION="-KSU-$BRANCH-$KMI_GENERATION-$COMREV-rio-$VARIANT"
+    
+    echo "--- Building KSU for $DEVICE ---"
+    START=$(date +%s)
     make -j$JOBS -C $SRC_DIR O=$OUT_DIR rio_defconfig ${VARIANT}.config
     make -j$JOBS -C $SRC_DIR O=$OUT_DIR
+    echo "Build took: $(($(date +%s) - START)) seconds."
 }
 
 gen_anykernel() {
-    echo "--- Generating AnyKernel3 Zip (KSU) ---"
-    AK3_DIR="$TC_DIR/RIO/$DEVICE/AnyKernel3_KSU"
-    mkdir -p "$AK3_DIR"
+    AK3_DIR="$TC_DIR/RIO/work_ksu"
+    rm -rf "$AK3_DIR" && mkdir -p "$AK3_DIR"
     cp -af "$TC_DIR/AnyKernel3/"* "$AK3_DIR/"
     cp "$OUT_DIR/arch/arm64/boot/Image" "$AK3_DIR/"
     cp "$OUT_DIR/arch/arm64/boot/dtbo.img" "$AK3_DIR/"
     cp "$OUT_DIR/arch/arm64/boot/dts/vendor/qcom/yupik.dtb" "$AK3_DIR/dtb"
-    
-    ( cd "$AK3_DIR"
-        ZIP_NAME="RIO_KSU_$(date +%Y%m%d)_${VARIANT}.zip"
-        zip -r9 "$ZIP_NAME" * -x .git README.md
-        mkdir -p "$TC_DIR/RIO/FINAL"
-        mv "$ZIP_NAME" "$TC_DIR/RIO/FINAL/"
-    )
+    cd "$AK3_DIR" && zip -r9 "RIO_KSU_$(date +%Y%m%d)_${VARIANT}.zip" ./* -x .git* README.md
+    mkdir -p "$TC_DIR/RIO/FINAL" && mv *.zip "$TC_DIR/RIO/FINAL/" && cd "$SRC_DIR"
 }
 
-install_deps
-fetch_tools
+install_deps && fetch_tools
 rm -rf KernelSU drivers/kernelsu
 git switch susfs-rio || git checkout susfs-rio
 curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s susfs-rksu-master
-build_kernel "$1"
-gen_anykernel
+build_kernel "$1" && gen_anykernel
